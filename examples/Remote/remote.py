6a875be6c30fdb05f9c0d2860c919aff7bba0114
NStrijbosch <n.strijbosch@gmail.com>
2021-04-09 19:15:13 +0200

    Update remote.py

--- examples/Remote/remote.py
+++ examples/Remote/remote.py
@@ -1,19 +1,18 @@
 from hub2hub import Remote, ble_handler
 from time import sleep_ms
 
+# Initialize ble handler and a remote
 ble = ble_handler()
 Remote = Remote(ble)
-Remote.connect()
 
-RemoteLed = Remote.led
-RemoteLeft = Remote.left
-RemoteRight = Remote.right
+# connect to a remote: press green button on the remote
+Remote.connect()
 
 k = 0
 while True:
-    RemoteLed(k%11)
-    print('Left plus pressed: ', RemoteLeft.plus.is_pressed())
-    print('Right plus was pressed: ', RemoteRight.plus.was_pressed())
+    Remote.led(k%11)
+    print('Left plus pressed: ', Remote.left.plus.is_pressed())
+    print('Right plus was pressed: ', Remote.right.plus.was_pressed())
 
     k+=1
     sleep_ms(1000)
